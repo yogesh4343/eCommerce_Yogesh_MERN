fbce72ec42560d4b92473521aa9aa008c393f69e
yogesh <103855728+yogesh4343@users.noreply.github.com>
2024-02-21 04:27:13 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,130 +1,84 @@
-import './App.css';
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Components/Home/Home';
-import Contact from './Components/Contact/Contact'
-import ProductHome from './Components/Product/ProductHome';
-import About  from './Components/About/About'
-import SingleProductPage from './Components/Product/SingleProductPage';
-import MyProfile from './Components/MyProfile/MyProfile';
-import EditProfile from './Components/EditProfile/EditProfile';
-import Orders from './Components/Orders/Orders';
-import SignOut from './Components/SignOut/SignOut';
-import Login from './Components/SignOut/Login';
-import Register from './Components/SignOut/Register';
-import AddToCart from './Components/AddToCart/AddToCart';
-// import WishList from './Components/WishList/WishList';
-import { useDispatch, useSelector } from 'react-redux';
-import AddProduct from './Components/Product/AddProduct';
-import SearchBarHide from './SearchBarHide';
-import { useEffect } from 'react';
-// import store from "./Store/Store"
-import { getLoadUserReducer } from './Store/Slice/UserSlice';
-import CheckoutSteps from './Components/AddToCart/CheckOutSteps';
-import ShippingInfo from './Components/AddToCart/ShippingInfo';
-import OrderConfirm from './Components/AddToCart/OrderConfirm';
-import ProcessPayment from './Components/AddToCart/ProcessPayment';
-import PageNotFound from './Components/Home/PageNotFound';
-import ChangePassword from './Components/EditProfile/ChangePassword';
+const express = require('express')
+const dotenv = require("dotenv")
+const mongoose = require('mongoose')
+const cookieParser = require("cookie-parser")
+const router = require('./Routes/UserRoutes')
+const bodyParser = require('body-parser');
+const cors = require("cors");
 
-function App() {
-
-  const dispatch = useDispatch();
-  //   const { userDetail , loadUser }   = useSelector(state => state.userDetails);
-  const {  loadUser  }   = useSelector(state => state.userDetails);
-  // console.log(loadUser.status)
-  console.log(loadUser , loadUser.message)
-
-  // getLoadUserReducer
-
-  
-
-
-  useEffect(()=>{
-     dispatch(getLoadUserReducer())
-    
-  },[dispatch , loadUser.success   ])
-
-
-//   if (loadUser.success) {
-//    alert("You Are Logged In ...")
-// }
-
-
- 
-  return (
-    <div className="App">
-       
-       <BrowserRouter>
-       {/* {userDetail.success && <UserOptions user={userDetail.user} />} */}
-       {/* {userDetail.success } */}
-        <Routes>
-           <Route path='/' element={ <Home />} />
-
-           <Route path="/product/:id" element={ <ProductHome /> } />
-           <Route path="/product" element={ <ProductHome /> } />
-           
-            <Route path="/contact" element={ <Contact /> } />
-           <Route path="/about" element={ <About /> } />
-
-           <Route path="*" element={ <PageNotFound /> } />
-
-            <Route path="/singleProduct/:id" element={ <SingleProductPage />} />     
-           
-
-            {/* {loadUser.status   &&   <Route path='/myprofile' element={<MyProfile />} /> } */}
-              <Route path='/myprofile' element={<MyProfile />} /> 
-            {/* {loadUser.status   &&  <Route path='/editprofile' element={<EditProfile />} /> } */}
-             <Route path='/editprofile' element={<EditProfile />} /> 
-            {/* {loadUser.status   &&   <Route path='/orders' element={<Orders />} /> } */}
-             <Route path='/orders' element={<Orders />} /> 
-           
-
-             {/* {userDetail.success   &&  <Route path='/admin/product/new' element={<AddProduct />} /> } */}
-
-         
-
-             {loadUser.status   &&  <Route exact path='/admin/product/new' element={<AddProduct />} />  }
-           <Route path='/signout' element={<SignOut />} />
-         
-
-           <Route path='/login' element={<Login />} />
-           <Route path='/register' element={<Register />} /> 
-            
-
-
-         
-           
-           <Route path='/search' element={<SearchBarHide />} />
-
-
-           {/* <Route path='/wishlist' element={<WishList />} /> */}
+const path = require('path')
 
 
 
 
-           {/* {userDetail.success   && <Route path='/addtocart' element={<AddToCart />} /> } */}
-           {/* {loadUser.status   && <Route exact path='/addtocart' element={<AddToCart />} /> } */}
-            <Route path='/addtocart' element={<AddToCart />} /> 
-
-           {/* {loadUser.status   && <Route exact path='/password/update' element={<ChangePassword />} /> } */}
-           <Route exact path='/password/update' element={<ChangePassword />} /> 
-
-           {/* {loadUser.status   && <Route exact path='/checkoutsteps' element={<CheckoutSteps />} /> } */}
-            <Route exact path='/checkoutsteps' element={<CheckoutSteps />} /> 
-
-           {/* {loadUser.status   && <Route exact path='/shippingInfo' element={<ShippingInfo />} /> } */}
-             <Route exact path='/shippingInfo' element={<ShippingInfo />}  />
-             
-           {loadUser.status   && <Route exact path='/order/confirm' element={<OrderConfirm />} /> }
-           {loadUser.status   && <Route exact path='/process/payment' element={<ProcessPayment/>} /> }
-
-           
 
 
-        </Routes>
-    </BrowserRouter>
-    </div>
-  );
-}
+const app = express()
 
-export default App;
+app.use(cors({ origin: true, credentials: true }));
+
+dotenv.config(); 
+
+// connect to config -> config.env 
+
+// dotenv.config({path:"backend/config/config.env"})
+
+
+// static 
+// app.use(express.static(path.join(__dirname, '../../frontend/build')))
+
+// app.get('*' , function(req, res){
+//     res.sendFile(path.join(__dirname , "../../frontend/build/index.html"))
+// })  
+
+
+
+// app.use()   Start   +++++++++++++++++++++
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+// app.use()    End    ---------------------
+
+
+
+
+const user = require('./Routes/UserRoutes')
+const product = require('./Routes/ProductRoutes')
+const order = require('./Routes/OrderRoutes')
+// app.use()   Start   +++++++++++++++++++++
+
+app.use('/api' , user );
+app.use('/api' , product);
+app.use('/api' , order)
+
+// app.use()    End    ---------------------
+
+
+
+
+
+
+app.use(`/`,(req,res, next)=>{
+    console.log("Request was made");
+    res.send("Request was made");
+})
+
+
+
+
+// this is server Start   +++++++++++++++++++++
+
+mongoose.connect(process.env.DB_URI).then(()=>{
+    console.log("Connected to MongoDB PORTT" , process.env.PORT);
+}).then(()=>{
+    // app.listen("4000")
+    app.listen(process.env.PORT)
+}).catch((error)=>{
+    console.log(error)
+})
+
+// this is server End    ---------------------
