9be76dae7b50737a2f8c0b5cf77c002474307035
yogesh <103855728+yogesh4343@users.noreply.github.com>
2024-02-21 04:35:44 +0530

    Update NavBar.jsx

--- frontend/src/Assests/NavBar/NavBar.jsx
+++ frontend/src/Assests/NavBar/NavBar.jsx
@@ -1,16 +1,11 @@
 import React, {  useState } from 'react'
 import "../../App.css"
-// import SearchIcon from '@mui/icons-material/Search';
-// npm install @mui/icons-material
-// import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-// import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import Hamburger from 'hamburger-react'
 import { ProfileMenu } from './UserAccount';
 import Search from '@mui/icons-material/Search';
 import {  NavLink } from "react-router-dom";
 import SearchBarHide from '../../SearchBarHide';
-// import ScreenSearchDesktopSharpIcon from '@mui/icons-material/ScreenSearchDesktopSharp';
 import { useSelector } from 'react-redux';
 
 
@@ -25,8 +20,7 @@ const NavBar = () => {
   // const { userDetail , loadUser }   = useSelector(state => state.userDetails);
   const {cartDetails } = useSelector(state => state.cart)
   const {cartItems} = {...cartDetails}
-  // console.log(loadUser.status)
-  // console.log(`cartLength`, cartItems.length)
+
   
 
   const searchBtn = ()=>{
@@ -41,11 +35,6 @@ const NavBar = () => {
   const addToCartIcon = ()=>{
     console.log(`addToCartIcon`)
   }
-
-// useEffect(()=>{
-
-// },[loadUser])
-
 
 
   return (
@@ -86,37 +75,14 @@ const NavBar = () => {
 
             <div className="right  flex justify-end gap-4 cursor-pointer ">
                 <div className="search flex items-center  hover:text-red-600 relative" onClick={()=>searchBtn(setSearchBar(!searchBar))}> <Search /> 
-                {/* <div className="search flex items-center  hover:text-red-600 relative" onClick={searchBtn}> {searchBar ?  <ScreenSearchDesktopSharpIcon /> : <Search /> } */}
-                
-                {/* {searchBar ? <div className="absolute top-20 w-[70vw] shadow-2xl p-5 bg-blue-gray-100 right-[-10rem] sm:right-0   "> */}
-                  {/* <SearchBarHide /> */}
-                {/* </div> :  <Search />} */}
-
                  </div>
 
-
-                {/* <div className="search flex items-center  hover:text-red-600 " > <input type="text" className="text-black p-2 rounded-lg" placeholder='Search Here' />  </div> */}
-  
-               {/* <div className="heart flex items-center  hover:text-red-600 relative">  <NavLink to="/wishlist">    <FavoriteBorderIcon /> <span className="absolute bg-red left-3 bottom-8 text-center w-5 h-5 flex items-center justify-center rounded-md bg-blue-600 text-white  ">0</span> </NavLink>  </div>  */}
-
-
              <div className="cart flex items-center   hover:text-red-600 relative " onClick={addToCartIcon}>  <NavLink to="/addtocart">  <ShoppingCartIcon /> <span className="absolute bg-red left-3 bottom-8 text-center w-5 h-5 flex items-center justify-center rounded-md bg-blue-600 text-white  ">{cartItems.length}</span> </NavLink>   </div> 
-
-
-
-{/* {loadUser.status ?
-             <div className="cart flex items-center   hover:text-red-600 relative " onClick={addToCartIcon}>  <NavLink to="/addtocart">  <ShoppingCartIcon /> <span className="absolute bg-red left-3 bottom-8 text-center w-5 h-5 flex items-center justify-center rounded-md bg-blue-600 text-white  ">0</span> </NavLink>   </div>  : null } */}
 
 
                 <div className="user flex items-center   hover:text-red-600 ">   <ProfileMenu />
               </div>
 
-               
-
-              
-
-
-                
                  {navHide===true ?  <div className="hamburger cursor-pointer md:hidden w-12 " onClick={()=> setNavHide(false)} > <Hamburger /></div> :  <div className="cross cursor-pointer   md:hidden" onClick={()=>setNavHide(true)}> <Hamburger /></div> }
 
                  
